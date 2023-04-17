0c93d5f7d14a73f3624aa6a47c6a20c921fd5de9
Javier Castro <yoprogramo125@gmail.com>
2023-04-17 16:21:13 -0300

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    projectId: '',
-    appId: '',
-    storageBucket: '',
+    projectId: 'portfoliofront-jac',
+    appId: '1:1029183437100:web:f66a0d995c0c7d700d5489',
+    storageBucket: 'portfoliofront-jac.appspot.com',
     locationId: '',
-    apiKey: '',
-    authDomain: '',
-    messagingSenderId: '',
+    apiKey: 'AIzaSyATEnHbPa1OZcybXu0aOQMWll5meoE9AhI',
+    authDomain: 'portfoliofront-jac.firebaseapp.com',
+    messagingSenderId: '1029183437100',
   },
   production: true,
   URl:''
