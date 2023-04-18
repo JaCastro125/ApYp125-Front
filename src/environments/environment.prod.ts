34bcbaabe72146c294a52d1d1ce43a8370412bfc
Javier Castro <yoprogramo125@gmail.com>
2023-04-18 19:21:18 -0300

    cargando enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,4 @@
 export const environment = {
-  firebase: {
-    projectId: 'portfoliofront-jac',
-    appId: '1:1029183437100:web:f66a0d995c0c7d700d5489',
-    storageBucket: 'portfoliofront-jac.appspot.com',
-    locationId: '',
-    apiKey: 'AIzaSyATEnHbPa1OZcybXu0aOQMWll5meoE9AhI',
-    authDomain: 'portfoliofront-jac.firebaseapp.com',
-    messagingSenderId: '1029183437100',
-  },
   production: true,
-  URl:''
+  URl:'https://apyp125-portfolio-jac.onrender.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,28 +1,4 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  firebase: {
-    projectId: 'portfoliofront-jac',
-    appId: '1:1029183437100:web:f66a0d995c0c7d700d5489',
-    storageBucket: 'portfoliofront-jac.appspot.com',
-    locationId: '',
-    apiKey: 'AIzaSyATEnHbPa1OZcybXu0aOQMWll5meoE9AhI',
-    authDomain: 'portfoliofront-jac.firebaseapp.com',
-    messagingSenderId: '1029183437100',
-  },
   production: false,
-
-  URl:"http://localhost:8080",
-  
+  URl:"http://localhost:8080", 
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
