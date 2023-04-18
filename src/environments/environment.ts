d522b3743c73573d99e69f38363ff7bfc4a937bb
Javier Castro <yoprogramo125@gmail.com>
2023-04-18 20:06:35 -0300

    actualizando firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,15 @@
 export const environment = {
+
+  firebase: {
+    projectId: 'apyp125-portfolio',
+    appId: '1:605839997504:web:5945ac7fbbb5a3bc56bda5',
+    storageBucket: 'apyp125-portfolio.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyCcJhNXLN5zUacA4mxvWjGMBooXoT5ZRlk',
+    authDomain: 'apyp125-portfolio.firebaseapp.com',
+    messagingSenderId: '605839997504',
+  },
+
   production: false,
   URl:"http://localhost:8080", 
 };
